57bbf1c7f2ea12b06a931bcaf640bf3083a6c32e
Alan Platt <aplatt@equalexperts.com>
2018-01-13 13:20:09 +0000

    module for adding information to ssm

---
+++ modules/aws/ssm_params/domain_ssm.tf
@@ -0,0 +1,12 @@
+# Released under Apache licence v2.0 - Copyright and license notices must be preserved.
+# See the LICENCE file at the top-level directory of this repo or at
+# https://github.com/EqualExpertsMicroDC/terraform-stacks/blob/master/LICENSE
+
+variable "domain" { }
+
+resource "aws_ssm_parameter" "domain" {
+  name  = "Domain"
+  type  = "String"
+  value = "${var.domain}"
+  overwrite = true
+}
